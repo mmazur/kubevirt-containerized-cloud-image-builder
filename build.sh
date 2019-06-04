616ceb4de4b2f9ce3b41d84064196531778a9804
Mariusz Mazur <mmazur@redhat.com>
2019-06-04 09:59:48 +0200

    Use podman for login/push
    Travis uses Ubuntu, ubuntu doesn't get up to date podman/buildah pkgs.

--- build.sh
+++ build.sh
@@ -3,11 +3,11 @@
 set -e
 
 echo "Logging into $REGISTRY"
-echo "$REGISTRY_PASSWORD" | buildah login -u "$REGISTRY_USER" --password-stdin "$REGISTRY"
+echo "$REGISTRY_PASSWORD" | podman login -u "$REGISTRY_USER" --password-stdin "$REGISTRY"
 
 set -xe
 
 buildah bud -t $REGISTRY/$REGISTRY_USER/fedora:30 --build-arg OS_VER=30 image
 
-buildah push $REGISTRY/$REGISTRY_USER/fedora:30 $REGISTRY/$REGISTRY_USER/fedora:30
+podman push $REGISTRY/$REGISTRY_USER/fedora:30 $REGISTRY/$REGISTRY_USER/fedora:30
 
